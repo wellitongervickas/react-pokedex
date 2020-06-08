9ab0f66778624236af08303975d862bdb6dd2c65
welliton gervickas <welliton@meucredere.com.br>
2020-06-07 23:54:38 -0300

    First component test

---
+++ src/components/Badget/Badget.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Badget from '.';
+
+it('renders badget component', () => {
+  const { getByText, container } = render(<Badget>Hello World</Badget>);
+
+  expect(getByText('Hello World')).toBeInTheDocument();
+  expect(container.firstElementChild?.classList.contains('badget')).toBeTruthy();
+});
